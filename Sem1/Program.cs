c4c2e01ed7c0f85762a8ceb8278e4e57373f0f1f
Julia <juliaf92@mail.ru>
2022-11-29 22:33:57 +0300

    New

---
+++ Sem1/Program.cs
@@ -0,0 +1,11 @@
+﻿string? num1line = Console.ReadLine();
+string? num2line = Console.ReadLine();
+
+if(num1line!=null&&num2line!=null);
+{
+    int num1 = int.Parse(Num1Line);
+    int num2 = int.Parse(Num2Line);
+
+    int outResult = num1 * num2;
+    Console.WriteLine(outResult);
+}
