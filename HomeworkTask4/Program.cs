23beaae44f73b2cb76a0e8d19dc2fea50995257d
Julia <juliaf92@mail.ru>
2022-11-30 22:21:45 +0300

    Added files

---
+++ HomeworkTask4/Program.cs
@@ -0,0 +1,29 @@
+﻿string? Num1Line = Console.ReadLine();
+string? Num2Line = Console.ReadLine();
+string? Num3Line = Console.ReadLine();
+
+
+
+if (Num1Line != null && Num2Line != null && Num3Line != null)
+{
+    int num1 = int.Parse(Num1Line);
+    int num2 = int.Parse(Num2Line);
+    int num3 = int.Parse(Num3Line);
+   
+    if (num1 > num2)
+    {
+       if (num1 > num3)
+        Console.WriteLine("max"+num1);
+       else
+       Console.WriteLine("max"+num3);
+
+    }
+    else
+    {
+        if (num2 > num3)
+        Console.WriteLine("max"+num2);
+        else
+         Console.WriteLine("max"+num3);
+    }
+    
+}
