8a4cb7bb0758e847881b2d977976a7a6dbacc904
Julia <juliaf92@mail.ru>
2022-12-01 15:19:39 +0300

    Added a comments

--- HomeworkTask2/Program.cs
+++ HomeworkTask2/Program.cs
@@ -1,14 +1,18 @@
-﻿string? Num1Line = Console.ReadLine();
+﻿//Считываем данные с консоли
+string? Num1Line = Console.ReadLine();
 string? Num2Line = Console.ReadLine();
 
-
+//Проверяем, чтобы данные были не пустыми
 if (Num1Line != null && Num2Line != null)
 {
+//Парсим введенные числа
     int num1 = int.Parse(Num1Line);
     int num2 = int.Parse(Num2Line);
-   
+
+//Сравниваем числа 
     if (num1 > num2)
     {
+//Выводим данные в консоль
         Console.WriteLine("num1>num2");
     }
     else
