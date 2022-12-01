74de1f8d65a7633c86c28d4b65d354d87d7d897b
Julia <juliaf92@mail.ru>
2022-12-01 16:47:16 +0300

    Added a solution in the eight homework task

---
+++ HomeworkTask8/Program.cs
@@ -0,0 +1,23 @@
+﻿//Считываем данные с консоли
+string? Num1Line = Console.ReadLine();
+
+//Проверяем, чтобы данные были не пустыми
+if (Num1Line != null)
+{
+    //Парсим введенное число
+    int num1 = int.Parse(Num1Line);
+
+    //Вводим дополнительные переменные
+    int c = 2;
+    string outResult = string.Empty;
+
+    //Задаем условие для входа в цикл
+    while (c <= num1)
+{
+        outResult = outResult + c + ",";
+    c = c + 2;
+}
+    //Выводим данные в консоль
+    Console.WriteLine(outResult);
+
+}
