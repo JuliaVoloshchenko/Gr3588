061fd0a9567f33ab34d48d21b73da4ad6ea0c9a8
Julia <juliaf92@mail.ru>
2022-12-01 16:02:22 +0300

    Added a solution in the six homework task

---
+++ HomeworkTask6/Program.cs
@@ -0,0 +1,23 @@
+﻿//Считываем данные с консоли
+string? Num1Line = Console.ReadLine();
+
+//Проверяем, чтобы данные были не пустыми
+if (Num1Line != null)
+{
+    //Парсим введенное число
+    int num1 = int.Parse(Num1Line);
+
+    //Вводим дополнительную переменную
+    int c = num1 % 2;
+
+    //Задаем условие
+    if (c == 0)
+
+        Console.WriteLine("Четное");
+
+    else
+
+        Console.WriteLine("Нечетное");
+
+
+}
